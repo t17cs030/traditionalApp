c5983e21a10cec401727b4df05f9b0da692e1c8d
kobatoking <t17cs030@yamanashi.ac.jp>
2020-09-14 18:45:41 +0900

    [EDIT]検索ボタンを追加した(押すと選択項目の内容が表示される)(デバック用)

--- app/src/main/java/com/example/wineapp/SearchActivity.java
+++ app/src/main/java/com/example/wineapp/SearchActivity.java
@@ -3,8 +3,10 @@ package com.example.wineapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +39,45 @@ public class SearchActivity extends AppCompatActivity
                 color.setText(checkedButton.getText());
             }
         });
+
+        Button enter = findViewById(R.id.enter);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //押されているラジオボタン
+                RadioGroup color = (RadioGroup) findViewById(R.id.RadioGroup_Color);
+                int checkedRadioButtonId = color.getCheckedRadioButtonId();
+                RadioButton checkedButton = (RadioButton)findViewById(checkedRadioButtonId);
+
+                //赤ワインの味わい
+                Spinner spinner_taste_red_bottom = (Spinner) findViewById(R.id.spinner_taste_red_bottom);
+                Spinner spinner_taste_red_top = (Spinner) findViewById(R.id.spinner_taste_red_top);
+                String selected_red_bottom = (String) spinner_taste_red_bottom.getSelectedItem();
+                String selected_red_top = (String) spinner_taste_red_top.getSelectedItem();
+
+                //白ワインの味わい
+                Spinner spinner_taste_white_bottom = (Spinner) findViewById(R.id.spinner_taste_white_bottom);
+                Spinner spinner_taste_white_top = (Spinner) findViewById(R.id.spinner_taste_white_top);
+                String selected_white_bottom = (String) spinner_taste_white_bottom.getSelectedItem();
+                String selected_white_top = (String) spinner_taste_white_top.getSelectedItem();
+
+                //価格
+                Spinner spinner_price_bottom = (Spinner) findViewById(R.id.spinner_price_bottom);
+                Spinner spinner_price_top = (Spinner) findViewById(R.id.spinner_price_top);
+                String selected_price_bottom = (String) spinner_price_bottom.getSelectedItem();
+                String selected_price_top = (String) spinner_price_top.getSelectedItem();
+
+                String result
+                        = "色: " + checkedButton.getText() + "\n"
+                        + "味わい(赤): " + selected_red_bottom + "～" + selected_red_top + "\n"
+                        + "味わい(白): " + selected_white_bottom + "～" + selected_white_top + "\n"
+                        + "価格: " + selected_price_bottom + "～" + selected_price_top;
+                TextView result_text = findViewById(R.id.result);
+                result_text.setText(result);
+            }
+        });
+
+
         //今押されているラジオボタンのIDを取得(コメントアウト)
         //int checkedRadioButtonId = color.getCheckedRadioButtonId();
         //RadioButton checkedButton = (RadioButton)findViewById(checkedRadioButtonId);
